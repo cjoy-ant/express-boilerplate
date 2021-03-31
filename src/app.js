4241cc293e06d3f4607edb4cab386ead29e7e42c
Christine Antonio <christinejm.antonio@gmail.com>
2021-03-31 10:43:12 -0700

    install winston for loggin, logger.js, refractored errorHandler

--- src/app.js
+++ src/app.js
@@ -4,6 +4,7 @@ const morgan = require("morgan");
 const helmet = require("helmet");
 const cors = require("cors");
 const { NODE_ENV } = require("./config");
+const errorHandler = require("./error-handler");
 
 const app = express();
 
@@ -17,15 +18,6 @@ app.get("/", (req, res) => {
   res.send("Hello, world!");
 });
 
-app.use(function errorHandler(error, req, res, next) {
-  let response;
-  if (NODE_ENV === "production") {
-    response = { error: { message: "server error" } };
-  } else {
-    console.error(error);
-    response = { message: error.message, error };
-  }
-  res.status(500).json(response);
-});
+app.use(errorHandler);
 
 module.exports = app;
